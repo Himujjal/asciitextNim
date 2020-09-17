5bb63145a32a7fd285932ea7587e86e2496a1192
Himujjal <upadhyayahimujjal@gmail.com>
2020-09-18 03:25:32 +0530

    more changes

--- src/asciitext.nim
+++ src/asciitext.nim
@@ -1,13 +1,15 @@
 import strutils
 import strformat
 
-import buildStyle
+import asciitext/buildStyle
 
-type TextStyle = enum textStyle1, textStyle2, textStyle3
+type TextStyle =
+  enum textStyle1, textStyle2,
+  # textStyle3
 
 type InvalidCharacterError = ref object of CatchableError
 
-proc asciiText*(input: string, textStyle: TextStyle = textStyle1): string  {.raises: [InvalidCharacterError, ValueError] .} =
+proc asciiText*(input: string, textStyle: TextStyle = textStyle2): string  {.raises: [InvalidCharacterError, ValueError] .} =
   var validChars = true
   var invalidChar: char
 
@@ -21,11 +23,10 @@ proc asciiText*(input: string, textStyle: TextStyle = textStyle1): string  {.rai
   if validChars == false:
     raise InvalidCharacterError(msg: fmt"Fatal Error: Character '{invalidChar}' invalid. Only characters a-z,0-9 and newlines (\n) accepted")
   else:
-    return buildStyle2(inputText)
-  #  return case textStyle:
-  #   of textStyle1: buildStyle1(inputText)
-  # of textStyle2: buildStyle2(inputText)
-  # of textStyle3: buildStyle3(inputText)
+      case textStyle:
+      of textStyle1: return buildStyle1(inputText)
+      of textStyle2: return buildStyle2(inputText)
+    # of textStyle3: return buildStyle3(inputText)
 
 when isMainModule:
   echo asciiText("23h 23m 0s", textStyle2)
